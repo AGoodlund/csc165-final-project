3692057d67f3f02c910d6b7de90f238694207c6c
Aaron G <146310859+AGoodlund@users.noreply.github.com>
2025-03-14 12:01:16 -0700

    added link to more materials
    A public list of material ADSS characteristics

--- 165/tage/Utils.java
+++ 165/tage/Utils.java
@@ -374,7 +374,8 @@ public class Utils
 		DataBufferByte dataBuf = (DataBufferByte) raster.getDataBuffer();
 		return dataBuf.getData();
 	}
-
+//https://www.barradeau.com/nicoptere/dump/materials.html
+//a link to a table of different materials that can be used
 	// GOLD material - ambient, diffuse, specular, and shininess
 	public static float[] goldAmbient()  { return (new float [] {0.2473f,  0.1995f, 0.0745f, 1} ); }
 	public static float[] goldDiffuse()  { return (new float [] {0.7516f,  0.6065f, 0.2265f, 1} ); }
